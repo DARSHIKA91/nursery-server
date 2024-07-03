5c108dba1c8f95a1a7b4e6432e04646f08cde268
DARSHIKA91 <darshikaagrawal9175@gmail.com>
2024-07-03 12:15:20 +0530

    add

--- index.js
+++ index.js
@@ -179,11 +179,7 @@ app.delete("/plant/:id", (req, res)=>{
     })
 })
 
-app.use("*", (req, res)=>{
-    res.send(`<div>
-        <h1 style="text-align: center;">404 Not found>/h1>
-        </div>`)
-})
+
 
 const PORT = process.env.PORT
 
